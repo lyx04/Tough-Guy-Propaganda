54279e89edb4974495e1e0b4c3105f34bcdecb46
lyx <584719733@qq.com>
2017-11-28 19:29:15 +0800

    首页

---
+++ assets/scripts/index/index.js
@@ -0,0 +1,36 @@
+(function($) {
+	$(function() {
+		var mySwiper = new Swiper('#banner', {
+			direction: 'horizontal',
+			loop: true,
+		})
+		//		number数字转换
+		var numTop = $(".number").offset().top
+		var arr = [10, 2000, 600, 90, 50]
+		$(window).on("scroll", function(e) {
+			var el = $(window).scrollTop()
+			if(el + 50 > numTop) {
+					$(".number").find("li span").each(function(i, v) {
+						var spannum = $(".number").find("li span:first").html()
+						if(spannum > arr[0]) {
+							return 
+						} else {
+							var numOnly = arr[i] / 10
+							var this_num = $(v).html()
+							$(v).html()
+							$(v).html(this_num* 1 + numOnly )
+						}
+
+					})
+			}
+		})
+//		新闻动态的切换
+		$(".toggle").find("li").on("click",function(){
+			var index = $(this).index()
+			$(".toggle").find("li").removeClass("active")
+			$(this).addClass("active")
+			$(".toggle_first").hide()
+			$(".toggle_first").eq(index).show()
+		})
+	})
+})(jQuery)
